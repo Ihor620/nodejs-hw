0ad5fa919057dfaea03d4849c741273a22a6cd1b
IhorKM <11kozenkoihor@gmail.com>
2026-05-10 15:37:58 +0200

    fix: handle MongoDB connection error and fix returnDocument option

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -1,7 +1,13 @@
 import mongoose from 'mongoose';
 
 export const connectMongoDB = async () => {
-  await mongoose.connect(process.env.MONGO_URL);
-  // eslint-disable-next-line no-console
-  console.log('✅ MongoDB connection established successfully');
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    // eslint-disable-next-line no-console
+    console.log('✅ MongoDB connection established successfully');
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error('❌ MongoDB connection error:', err);
+    process.exit(1);
+  }
 };
