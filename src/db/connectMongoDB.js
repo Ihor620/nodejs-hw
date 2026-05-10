86440eeeeadb94e04d5cd92e3a31b9784c90e93a
IhorKM <11kozenkoihor@gmail.com>
2026-05-10 15:27:18 +0200

    Fix connectMongo

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -2,6 +2,6 @@ import mongoose from 'mongoose';
 
 export const connectMongoDB = async () => {
   await mongoose.connect(process.env.MONGO_URL);
-
+  // eslint-disable-next-line no-console
   console.log('✅ MongoDB connection established successfully');
 };
